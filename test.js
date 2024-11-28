f6fd9b454ea3d517bd2005d901caa1507ecf2755
LukinSV <lukins.ya@yandex.ru>
2024-11-28 13:17:37 +0300

    28.11.24 13.15

--- test.js
+++
@@ -1,6 +0,0 @@
-const arrUpperCase = (delimiter) => {
-  return "молоко сахар бетон бабушка".split(delimiter).map((elem) => {
-    return elem.toUpperCase();
-  });
-};
-console.log(arrUpperCase(" "));
